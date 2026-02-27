39c5b8414ccc38d2a96b587f5663825fc08f0d13
lanzione1 <236029920@qq.com>
2026-02-28 03:29:22 +0800

    fix: 使用 Hash 路由模式,兼容 GitHub Pages 静态托管

--- apps/customer-h5/src/router/index.ts
+++ apps/customer-h5/src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useUserStore } from '../stores/user'
 import BookingPage from '../pages/BookingPage.vue'
 import SuccessPage from '../pages/SuccessPage.vue'
@@ -60,7 +60,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
